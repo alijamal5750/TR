fa5702813900a20b50ed841a43cf79a2b72f1905
alijamal5750 <118554626+alijamal5750@users.noreply.github.com>
2023-02-23 00:48:50 +0300

    Update productService.js

--- services/productService.js
+++ services/productService.js
@@ -21,7 +21,7 @@ await sharp(req.files.imageCover[0].buffer)
 .resize(2000,1333)
 .toFormat('jpeg')
 .jpeg({quality:95})
-.toFile(`./uploads/products/${imageCoverFileName}`);
+.toFile(`uploads/products/${imageCoverFileName}`);
 
 // save image after processing to the DB : 
 req.body.imageCover=imageCoverFileName;
